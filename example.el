7b00569eab0ac5157ad2672104e0e05c34764ebc
Damien Garaud <damien.garaud@gmail.com>
2016-10-11 23:03:53 +0200

    add an exemple file

---
+++ example.el
@@ -0,0 +1,19 @@
+;;; Example.el
+;;
+;;
+;;; Code:
+
+
+(add-to-list 'load-path ".")
+
+(require 'emaze)
+
+(pp (emaze-make-grid 5 5))
+
+
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+
+;;; example.el ends here
