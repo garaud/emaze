8bbd62c365a33a11cf81c3f6d8e29d0e212f67ca
Damien Garaud <damien.garaud@gmail.com>
2016-10-14 17:59:26 +0200

    rename all tests
    add the 'emaze-test-' prefix

--- test/grid-test.el
+++ test/grid-test.el
@@ -3,7 +3,7 @@
 (require 'emaze)
 (require 'ert)
 
-(ert-deftest compute-cell-number ()
+(ert-deftest emaze-test-compute-cell-number ()
   "Compute the number of grid cells."
   (should (equal (emaze-ncell (emaze-make-grid 5 5)) 25))
   (should (equal (emaze-ncell (emaze-make-grid 7 3)) 21))
@@ -11,18 +11,18 @@
   (should (equal (emaze-ncell (emaze-make-grid 0 0))  0))
   (should (equal (emaze-ncell (emaze-make-grid 2 2)) 4)))
 
-(ert-deftest compute-height ()
+(ert-deftest emaze-test-compute-height ()
   "Compute the height of a grid."
   (should (equal (emaze-height (emaze-make-grid 5 2)) 5))
   (should (equal (emaze-height (emaze-make-grid 1 10)) 1)))
 
-(ert-deftest compute-width ()
+(ert-deftest emaze-test-compute-width ()
   "Compute the width of a grid."
   (should (equal (emaze-width (emaze-make-grid 5 2)) 2))
   (should (equal (emaze-width (emaze-make-grid 1 1)) 1))
   (should (equal (emaze-width (emaze-make-grid 1 10)) 10)))
 
-(ert-deftest get-cell ()
+(ert-deftest emaze-test-get-cell ()
   "Get the cell for a specific coordinate."
   (let ((grid (emaze-make-grid 4 4)))
     (should (not (ht-empty? (emaze-get-cell 2 2 grid))))
