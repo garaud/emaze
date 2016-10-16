11ec3bc606d4dd27a103f6ef2b737f6f019f16f5
Damien Garaud <damien.garaud@gmail.com>
2016-10-16 09:53:52 +0200

    update the example file

--- example.el
+++ example.el
@@ -11,8 +11,12 @@
 (pp (emaze-make-grid 5 5))
 
 (setq grid (emaze-make-grid 5 5))
+(setq cell (emaze-make-cell 1 2))
+
 
 (ht-items (emaze-get-cell 1 4 grid))
+
+(emaze-cell-neighbors cell grid)
 
 
 ;; Local Variables:
